5269c99e82fec4cd1548deab5fbf00dad6425c20
abdrasulov <abdrasulov@gmail.com>
2023-05-29 15:47:39 +0600

    Make Chain class regular

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
@@ -1,21 +1,37 @@
 package io.horizontalsystems.ethereumkit.models
 
-enum class Chain(
+open class Chain(
     val id: Int,
     val coinType: Int,
     val gasLimit: Long,
     val syncInterval: Long,
     val isEIP1559Supported: Boolean
 ) {
-    Ethereum(1, 60, 2_000_000, 15, true),
-    BinanceSmartChain(56, 60, 10_000_000, 15, false),
-    Polygon(137, 60, 10_000_000, 15, true),
-    Optimism(10, 60, 10_000_000, 15, false),
-    ArbitrumOne(42161, 60, 10_000_000, 15, false),
-    Avalanche(43114, 60, 10_000_000, 15, true),
-    Gnosis(100, 60, 10_000_000, 15, true),
-    Fantom(250, 60, 10_000_000, 15, false),
-    EthereumGoerli(5, 1, 10_000_000, 15, true);
+    object Ethereum : Chain(1, 60, 2_000_000, 15, true)
+    object BinanceSmartChain : Chain(56, 60, 10_000_000, 15, false)
+    object Polygon : Chain(137, 60, 10_000_000, 15, true)
+    object Optimism : Chain(10, 60, 10_000_000, 15, false)
+    object ArbitrumOne : Chain(42161, 60, 10_000_000, 15, false)
+    object Avalanche : Chain(43114, 60, 10_000_000, 15, true)
+    object Gnosis : Chain(100, 60, 10_000_000, 15, true)
+    object Fantom : Chain(250, 60, 10_000_000, 15, false)
+    object EthereumGoerli : Chain(5, 1, 10_000_000, 15, true)
 
     val isMainNet = coinType != 1
+    val name: String
+        get() = javaClass.simpleName
+
+    companion object {
+        fun values() = arrayOf(
+            Ethereum,
+            BinanceSmartChain,
+            Polygon,
+            Optimism,
+            ArbitrumOne,
+            Avalanche,
+            Gnosis,
+            Fantom,
+            EthereumGoerli
+        )
+    }
 }
